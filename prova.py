faf180e0ab92d4fdd267f0c615b36c13198f5b78
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2025-01-19 19:55:21 -0400

    teste final

---
+++ prova.py
@@ -0,0 +1,6 @@
+import sys
+from PyQt6.QtDesigner import QDesignerWidgetBoxInterface, QDesignerCustomWidgetInterface, QPyDesignerTaskMenuExtension
+
+def on_click();
+    print("Botão foi clicado!!!")
+
